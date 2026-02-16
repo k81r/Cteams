f243d8dbc189d959533c69665e7bb21aeb5a0ce6
y25518 <y25518@iwate-it.ac.jp>
2026-02-16 16:01:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let timerId;
 let turn = 0;   // 0 => 1p, 1 => 2p
 let gap = [ 0, 0 ];   // 5秒との時間差を入れる用
 let p1_hp = 100;
-let p2_hp = 100;
+let p2_hp = 10;
 
 // タイマー更新
 function updateTime() {
@@ -95,5 +95,10 @@ function DMcalc(attackerId, gap, diff) {
 }
 // 勝敗判定
 function judge(loserId) {
-    document.getElementById(`${loserId}`).style.width = 0 + "%";
+    document.getElementById(`p${++loserId}-hp`).style.width = 0 + "%";
+    document.getElementById("goal-time").textContent = "K.O";
+    center = document.getElementById('center');
+    center.innerHTML = '<input id="restart" class="btn" type="button" value="もう一戦"><input id="quit" class="btn" type="button" value="やめる">';
+    restart.addEventListener('click', () => {location.reload()});   //ページを再読み込み
+    puit.addEventListener('click', () => {});       //ゲームトップに戻る
 }
